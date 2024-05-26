92e5b69fd920a87953b3376da41ea3b21c3bb048
Brian Keeley <imbriankeeley@gmail.com>
2024-05-25 21:21:24 -0400

    Created new gameBoard initialization logic

--- ttt.js
+++ ttt.js
@@ -2,16 +2,38 @@
 const gameBoard = (() => {
     const rows = 3;
     const columns = 3;
-    const board = [];
+    const board = Array.from(Array(rows), () => Array(columns).fill(''));
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for(let j = 0; j < columns; j++) {
-            board[i][j] = j;
+    const printBoard = () => {
+        console.log(board.map(row => row.join(' | ')).join('\n---------\n'));
+    };
+
+    const updateBoard = (row, column, marker) => {
+        if (board[row][column] === '') {
+            board[row][column] = marker;
+            return true;
         }
-    }
+        return false;
+    };
 
-})
+    const resetBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                board[i][j] = '';
+            }
+        }
+    };
+
+    return {
+        printBoard,
+        updateBoard,
+        resetBoard,
+        board
+    };
+})();
+
+
+
 
 
 function Player(name, marker) {
